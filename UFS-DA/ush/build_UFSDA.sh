613edbf8106dd123f56f4e1e6b50de0424634d7a
Cory Martin <cory.r.martin@noaa.gov>
2021-12-02 11:30:53 -0500

    Create build_UFSDA.sh

---
+++ UFS-DA/ush/build_UFSDA.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# build_UFSDA.sh
+# 1 - determine if on supported host
+# 2 - load modules
+# 3 - run ecbuild
+# 4 - build
+# 5 - optional, run unit tests
+
+set -ex
+
+cd ..
+pwd=$(pwd)
+
+run_tests=${1:-"NO"}
+dir_root=${2:-$pwd}
+
+# determine machine
+if [[ -d /scratch1 ]] ; then
+    . /apps/lmod/lmod/init/sh
+    target=hera
+elif [[ -d /work ]]; then
+    . $MODULESHOME/init/sh
+    target=orion
+else
+    echo "unknown target = $target"
+    exit 9
+fi
+
+dir_modules=$dir_root/modulefiles
+
+# remove/create directories
+[ -d $dir_root/exec ] || mkdir -p $dir_root/exec
+
+rm -rf $dir_root/build
+mkdir -p $dir_root/build
+cd $dir_root/build
+
+# load modules
+module purge
+module use $dir_modules
+module load UFSDA/$target
+
+# run ecbuild
+$ECBUILD_CMD ../sorc/
+
+# run make
+make -j8
+
+# if option is set, run ctests
+if [ $run_tests = "YES" ]; then
+    ctest
+fi
