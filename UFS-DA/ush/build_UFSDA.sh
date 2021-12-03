4015ce58e316411c5ee097562eb622fe525ffaf3
Cory Martin <cory.r.martin@noaa.gov>
2021-12-03 14:44:02 -0600

    Addressed further comments

--- UFS-DA/ush/build_UFSDA.sh
+++ UFS-DA/ush/build_UFSDA.sh
@@ -1,43 +1,32 @@
 #!/bin/bash
 # build_UFSDA.sh
-# 1 - determine if on supported host
-# 2 - load modules
-# 3 - run ecbuild
-# 4 - build
-# 5 - optional, run unit tests
+# 1 - determine if on supported host, load modules if so
+# 2 - run ecbuild
+# 3 - build
+# 4 - optional, run unit tests
 
 set -eux
 
 dir_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
-# determine machine
-if [[ -d /scratch1 ]] ; then
-    . $MODULESHOME/init/sh
-    target=hera
-elif [[ -d /work ]]; then
-    . $MODULESHOME/init/sh
-    target=orion
-else
-    echo "unknown target"
-    exit 9
-fi
-
 dir_modules=$dir_root/modulefiles
 
+if [[ ${BUILD_TARGET:-} = "orion" -o ${BUILD_TARGET:-} = "hera"]]; then
+    set +x
+    source $MODULESHOME/init/sh
+    module purge
+    module use $dir_modules
+    module load UFSDA/${BUILD_TARGET}
+    module list
+    set -x
+fi
+
 # remove/create directories
-[ -d $dir_root/exec ] || mkdir -p $dir_root/exec
+[ -d $dir_root/bin ] || mkdir -p $dir_root/bin
 
 rm -rf $dir_root/build
 mkdir -p $dir_root/build
 cd $dir_root/build
-
-# load modules
-set +x
-module purge
-module use $dir_modules
-module load UFSDA/$target
-module list
-set -x
 
 # run ecbuild
 ecbuild -DMPIEXEC_EXECUTABLE=$MPIEXEC_EXEC -DMPIEXEC_NUMPROC_FLAG=$MPIEXEC_NPROC ../sorc/
@@ -46,6 +35,6 @@ ecbuild -DMPIEXEC_EXECUTABLE=$MPIEXEC_EXEC -DMPIEXEC_NUMPROC_FLAG=$MPIEXEC_NPROC
 make -j ${BUILD_JOBS:-8} VERBOSE=${BUILD_VERBOSE:-}
 
 # link executables to exec dir
-ln -sf $dir_root/build/bin/fv3jedi* $dir_root/exec/.
+ln -sf $dir_root/build/bin/fv3jedi* $dir_root/bin/.
 
 exit 0
