1651cf0b2f980afcacd4a574ad17ad37daef2dd7
RussTreadon-NOAA <26926959+RussTreadon-NOAA@users.noreply.github.com>
2024-01-30 12:50:42 -0500

    Merge feature/gdas-validation into develop (#109)
    ---------
    
    Co-authored-by: Cory Martin <cory.r.martin@noaa.gov>

--- GDAS-validation/setup_workspace.sh
+++ GDAS-validation/setup_workspace.sh
@@ -58,7 +58,7 @@ if [ $machine = orion ]; then
   ICSDir=${ICSDir:-/work2/noaa/da/cmartin/UFO_eval/data/para/output_ufo_eval_aug2021}
 elif [ $machine = hera ]; then
   workdir=${workdir:-/scratch1/NCEPDEV/stmp2/$LOGNAME/gdas-validation}
-  ICSDir=${ICSDir:-/scratch1/NCEPDEV/da/Cory.R.Martin/blah/blah}
+  ICSDir=${ICSDir:-/scratch1/NCEPDEV/da/Cory.R.Martin/UFO_eval/data/para/output_ufo_eval_aug2021}
 else
    echo "Machine " $machine "not found"
    exit 1
@@ -71,31 +71,44 @@ mkdir -p $workdir
 if [ $clone = "YES" ]; then
   cd $workdir
   echo "Cloning global-workflow at $workdir/global-workflow"
-  git clone https://github.com/noaa-emc/global-workflow.git
-  cd global-workflow/sorc
-  ./checkout.sh -g
-  rm -rf gsi_enkf.fd
-  git clone --recursive https://github.com/NOAA-EMC/GSI.git gsi_enkf.fd
-  git clone --recursive https://github.com/NOAA-EMC/GDASApp.git gdas.cd
+  git clone --recursive https://github.com/noaa-emc/global-workflow.git
+  echo "Checkout GSI feature/gdas-validation"
+  cd $workdir/global-workflow/sorc/gsi_enkf.fd
+  git checkout feature/gdas-validation
 fi
 
 #--- build GDASApp and GSI ---
 if [ $build = "YES" ]; then
-  cd $workdir/global-workflow/sorc/gsi_enkf.fd/ush
-  echo "Building GSI in $workdir/global-workflow/sorc/gsi_enkf.fd/"
+  logs_dir="${workdir}/global-workflow/sorc/logs"
+  if [[ ! -d "${logs_dir}" ]]; then
+      echo "Create logs folder, ${logs_dir}"
+      mkdir "${logs_dir}" || exit 1
+  fi
+  sorc_dir="${workdir}/global-workflow/sorc"
+  cd ${sorc_dir}
+  echo "Building GSI in ${sorc_dir}/gsi_enkf.fd"
   echo "Build begin: `date`"
-  echo "Build log: $workdir/build_gsi.log"
-  ./build.sh > $workdir/build_gsi.log 2>&1
+  echo "Build log: ${logs_dir}/build_gsi_enkf.log"
+  ./build_gsi_enkf.sh > ${logs_dir}/build_gsi_enkf.log 2>&1
+  err=0
+  err=$?
+  if (( err != 0 )); then
+      echo "GSI build abnormal exit $err.  Check ${logs_dir}/build_gsi_enkf.log"
+      exit $err
+  fi
   echo "Build complete: `date`"
-  cd $workdir/global-workflow/sorc/gdas.cd
-  echo "Building GDASApp in $workdir/global-workflow/sorc/gdas.cd"
+  echo "Building GDASApp in ${sorc_dir}/gdas.cd"
   echo "Build begin: `date`"
-  echo "Build log: $workdir/build_gdasapp.log"
-  WORKFLOW_BUILD="ON" ./build.sh > $workdir/build_gdasapp.log 2>&1
+  echo "Build log: ${logs_dir}/build_gdas.log"
+  ./build_gdas.sh > ${logs_dir}/build_gdas.log 2>&1
+  err=$?
+  if (( err != 0 )); then
+      echo "GDASApp build abnormal exit $err.  Check ${logs_dir}/build_gdas.log"
+      exit $err
+  fi
   echo "Build complete: `date`"
-  cd $workdir/global-workflow/sorc/
   echo "Link workflow"
-  ./link_workflow.sh
+  ${sorc_dir}/link_workflow.sh
   # copy workflow default config files
   echo "Staging configuration files"
   mkdir -p $workdir/gdas_config
@@ -103,34 +116,38 @@ if [ $build = "YES" ]; then
   # copy files that need to be overwritted from default
   cp -rf $mydir/gdas_config/* $workdir/gdas_config/.
   # copy yamls that need to be overwritten for JEDI gdas-validation
-  cp -rf $mydir/gdas_config/3dvar_dripcg.yaml $workdir/global-workflow/sorc/gdas.cd/parm/atm/variational/
+  cp -rf $mydir/gdas_config/3dvar_drpcg.yaml $workdir/global-workflow/sorc/gdas.cd/parm/atm/variational/
+  # copy scripts that need to be overwritten for GSI gdas-validation
+  cp -rf $mydir/gdas_config/exglobal_atmos_analysis.sh $workdir/global-workflow/scripts/
 fi
 
 #--- setup default experiment within workflow
 if [ $setup = "YES" ]; then
-  module use $workdir/global-workflow/sorc/gdas.cd/modulefiles
-  module load GDAS/$machine
+  source $workdir/global-workflow/ush/detect_machine.sh
+  source $workdir/global-workflow/ush/module-setup.sh
+  module use $workdir/global-workflow/modulefiles
+  module load module_gwsetup.${MACHINE_ID}
   cd $workdir/global-workflow/workflow
   # setup_expt variables
   IDATE=2021080100
   EDATE=2021080200
-  RESDET=768
+  RESDETATMOS=768
   CDUMP=gdas
   PSLOT=${EXPNAME:-"gdas_eval"}
   CONFIGDIR=$workdir/gdas_config
-  COMROT=$workdir/comrot
+  COMROOT=$workdir/comroot
   EXPDIR=$workdir/expdir
   ICSDIR=$ICSDir/$IDATE
   rm -rf $EXPDIR/${PSLOT}*
-  rm -rf $COMROT/${PSLOT}*
+  rm -rf $COMROOT/${PSLOT}*
   # make two experiments, one GSI, one JEDI
   set -x
   ./setup_expt.py gfs cycled --idate $IDATE --edate $EDATE --app ATM --start warm --gfs_cyc 0 \
-    --resdet $RESDET  --nens 0 --cdump $CDUMP --pslot ${PSLOT}_GSI --configdir $CONFIGDIR \
-    --comrot $COMROT --expdir $EXPDIR --yaml $CONFIGDIR/config_gsi.yaml --icsdir $ICSDIR
+    --resdetatmos $RESDETATMOS  --nens 0 --cdump $CDUMP --pslot ${PSLOT}_GSI --configdir $CONFIGDIR \
+    --comroot $COMROOT --expdir $EXPDIR --yaml $CONFIGDIR/config_gsi.yaml --icsdir $ICSDIR
   ./setup_expt.py gfs cycled --idate $IDATE --edate $EDATE --app ATM --start warm --gfs_cyc 0 \
-    --resdet $RESDET  --nens 0 --cdump $CDUMP --pslot ${PSLOT}_JEDI --configdir $CONFIGDIR \
-    --comrot $COMROT --expdir $EXPDIR --yaml $CONFIGDIR/config_jedi.yaml --icsdir $ICSDIR
+    --resdetatmos $RESDETATMOS  --nens 0 --cdump $CDUMP --pslot ${PSLOT}_JEDI --configdir $CONFIGDIR \
+    --comroot $COMROOT --expdir $EXPDIR --yaml $CONFIGDIR/config_jedi.yaml --icsdir $ICSDIR
   set +x
   # setup the two XMLs
   ./setup_xml.py $EXPDIR/${PSLOT}_GSI
@@ -141,8 +158,6 @@ if [ $setup = "YES" ]; then
   # copy run_job configuration to each EXPDIR
   cp $mydir/config_example_gsi.sh $EXPDIR/${PSLOT}_GSI/config_gsi.sh
   cp $mydir/config_example_jedi.sh $EXPDIR/${PSLOT}_JEDI/config_jedi.sh
-  # link C768 GSIBEC to GSI EXPDIR
-  ln -fs $ICSDir/global_berror.l127y1538.f77 $EXPDIR/${PSLOT}_GSI/global_berror.l127y1538.f77
   # link backgrounds
   # the ICSDIR links the restarts, we also need the GSI inputs
   PDY=${IDATE:0:8}
@@ -153,16 +168,16 @@ if [ $setup = "YES" ]; then
   GDATE=$(date --utc +%Y%m%d%H -d "${PDY} ${cyc} - 6 hours")
   gPDY=${GDATE:0:8}
   gcyc=${GDATE:8:2}
-  mkdir -p ${COMROT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/
-  mkdir -p ${COMROT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/analysis/atmos/
+  mkdir -p ${COMROOT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/
+  mkdir -p ${COMROOT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/analysis/atmos/
   # below assumes the old com structure for the input data
-  echo "Linking backgrounds to ${COMROT}/${PSLOT}_GSI/"
+  echo "Linking backgrounds to ${COMROOT}/${PSLOT}_GSI/"
   # only f006, no FGAT in JEDI
-  #ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*atmf* ${COMROT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
-  #ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*sfcf* ${COMROT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
-  ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*atmf006* ${COMROT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
-  ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*sfcf006* ${COMROT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
-  ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*abias* ${COMROT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/analysis/atmos/.
+  #ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*atmf* ${COMROOT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
+  #ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*sfcf* ${COMROOT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
+  ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*atmf006* ${COMROOT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
+  ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*sfcf006* ${COMROOT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/model_data/atmos/history/.
+  ln -sf $ICSDIR/gdas.${gPDY}/${gcyc}/atmos/gdas*abias* ${COMROOT}/${PSLOT}_GSI/gdas.${gPDY}/${gcyc}/analysis/atmos/.
   # this is so the gdasatmanlfinal job completes successfully
   sed -e '/self\.jedi2fv3inc/s/^/#/g' -i $workdir/global-workflow/ush/python/pygfs/task/atm_analysis.py
 fi
